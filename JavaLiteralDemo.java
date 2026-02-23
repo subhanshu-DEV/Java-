37d2d934ee14d0c5a276c88daece159f1e0a57c5
subhanshu singh <subhanshusingh504@gmail.com>
2026-02-23 18:04:20 +0530

    Added Java Literals program

---
+++ JavaLiteralDemo.java
@@ -0,0 +1,14 @@
+public class JavaLiteralDemo {
+    public static void main(String[] args) {
+
+        int decimal = 25;
+        int binary = 0b11001;
+        float floatNum = 5.5f;
+        char letter = 'A';
+
+        System.out.println("Decimal: " + decimal);
+        System.out.println("Binary: " + binary);
+        System.out.println("Float: " + floatNum);
+        System.out.println("Char: " + letter);
+    }
+}
