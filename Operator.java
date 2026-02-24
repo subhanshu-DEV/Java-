e267f436b23bff2ed6087c513799f1506174bdc1
subhanshu singh <subhanshusingh504@gmail.com>
2026-02-24 13:24:49 +0530

    Added Operator program(Arithmetic operations in java)

---
+++ Operator.java
@@ -0,0 +1,12 @@
+public class Operator{
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 8;
+        System.out.println("Addition=" + (a + b));
+        System.out.println("Subtraction="  + (a - b));
+        System.out.println("Divosion="+ (a / b));
+        System.out.println("Multiplication="+ (a * b));
+
+
+    }
+}
