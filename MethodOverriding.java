ca32681a2f20c7c7c73fc451160714ec17263c57
subhanshu singh <subhanshusingh504@gmail.com>
2026-03-17 12:47:33 +0530

    Added method overriding example using Animal and Dog

---
+++ MethodOverriding.java
@@ -0,0 +1,22 @@
+class Animal {
+
+    void sound() {
+        System.out.println("Animal makes sound");
+    }
+}
+
+class Dog extends Animal {
+
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Main {
+
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+        d.sound();
+    }
+}
